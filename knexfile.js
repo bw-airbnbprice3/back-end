bd873062e2d6dad1297f2169a5560c925910fa38
cjgodfather <chaoji@Chaos-MacBook-Pro.local>
2019-11-18 15:51:47 -0500

    add pg support

--- knexfile.js
+++ knexfile.js
@@ -18,21 +18,16 @@ module.exports = {
         conn.run("PRAGMA foreign_keys = ON", done);
       }
     }
-  }
+  },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
+  production: {
+    client: "pg",
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: "./data/migrations"
+    },
+    seeds: {
+      directory: "./data/seeds"
+    }
+  }
 };
